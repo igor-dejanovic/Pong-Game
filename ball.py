ee2c5fbdb04f029564565f1a56f178a3d73b6e4c
igor-dejanovic <igor.dejanovic@sigmoid.rs>
2023-04-15 18:29:37 +0200

    almost done

---
+++ ball.py
@@ -0,0 +1,28 @@
+from turtle import Turtle
+
+
+class Ball(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.color("white")
+        self.speed("fastest")
+        self.goto(0, 0)
+        self.x_move = 10
+        self.y_move = 10
+
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+
+    def reset_postion(self):
+        self.goto(0, 0)
+        self.paddle_bounce()
+    def bounce(self):
+        self.y_move *= -1
+
+    def paddle_bounce(self):
+        self.x_move *= -1
