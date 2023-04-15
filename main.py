ab8c1c2c334ef4958433acd0666d54d690cec565
igor-dejanovic <igor.dejanovic@sigmoid.rs>
2023-04-15 18:29:08 +0200

    almost done

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from turtle import Screen
 from paddle import Paddle
+from scoreboard import Scoreboard
+from ball import Ball
+import time
+
+scoreboard = Scoreboard()
+ball = Ball()
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
@@ -19,6 +25,23 @@ screen.onkeypress(key="s", fun=l_paddle.move_down)
 game_is_on = True
 
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.paddle_bounce()
+        ball.speed() + 10
+
+    if ball.xcor() > 400:
+        ball.reset_postion()
+        scoreboard.l_point()
+
+    if ball.xcor() < -400:
+        ball.reset_postion()
+        scoreboard.r_point()
+
 
 screen.exitonclick()
